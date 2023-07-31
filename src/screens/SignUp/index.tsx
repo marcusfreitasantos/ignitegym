17360b82ef3beffdc77b05e4b8bfa99b32212b45
Marcus Freitas <marcusfreitasantos@gmail.com>
2023-07-31 11:54:58 -0300

    interface das telas de login e cadastro

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import { VStack, Image, Center, Text, Heading, ScrollView } from "native-base";
+import LogoSvg from "../../assets/logo.svg";
+import BackgroundImg from "../../assets/background.png";
+import Input from "@components/Input";
+import Button from "@components/Button";
+
+export default function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10}>
+        <Image
+          source={BackgroundImg}
+          alt="pessoas treinando na academia"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+
+          <Input placeholder="Nome" />
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+          <Button title="Criar e Acessar" />
+        </Center>
+
+        <Button mt={24} title="Voltar para Login" variant="outline" />
+      </VStack>
+    </ScrollView>
+  );
+}
